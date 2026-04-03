b6202b73a21ac69eefc6b77cd1f08baa2d9e8ed0
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2026-04-03 08:53:24 -0500

    mejorando el manejo de files

--- configs/helpers.js
+++ configs/helpers.js
@@ -7,3 +7,12 @@ export function randomFileName(length = 30) {
   }
   return result;
 }
+
+export const sanitizeFileName = (filename) => {
+  if (!filename) return '';
+  // Reemplazar caracteres no deseados
+  return filename
+    .replace(/[^a-zA-Z0-9.\u00f1\u00d1\-_]/g, '_') // Reemplaza caracteres especiales
+    .replace(/\s+/g, '_') // Reemplaza espacios
+    .replace(/_{2,}/g, '_'); // Evita múltiples guiones bajos seguidos
+};
